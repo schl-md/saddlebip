8270b2bf4c59d9b9f32d36a592b507bb90f3763d
chirk <mqyhlkahu@gmail.com>
2024-09-11 09:58:43 -0500

    Initial commit.

---
+++ src/main/java/com/trufflecat/saddlebip/App.java
@@ -0,0 +1,10 @@
+package com.trufflecat.saddlebip;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
